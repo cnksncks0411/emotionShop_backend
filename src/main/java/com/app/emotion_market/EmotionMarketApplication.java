6402b34c91ab5fecc1b874b9c1cc733837e43635
DESKTOP-MRU1UO2\A <cnksncks0411@naver.com>
2025-06-30 22:57:53 +0900

    최초커밋

---
+++ src/main/java/com/app/emotion_market/EmotionMarketApplication.java
@@ -0,0 +1,13 @@
+package com.app.emotion_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/emotion_market/EmotionMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.emotion_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
